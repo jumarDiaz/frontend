0004a32328604a6533d373c9915e68be99dc8857
jumarDiaz <jjareddiaz@uniguajira.edu.co>
2023-03-22 21:53:26 -0500

    agregando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { ContentComponent } from './components/layout/content/content.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { SectionComponent } from './components/layout/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
